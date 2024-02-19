fd7db3810ffc07532e44e072fa76f0d17005ee67
Madhurima <madhurimavispute61@gmail.com>
2024-02-19 19:43:54 +0530

    new

--- main.js
+++ main.js
@@ -3,25 +3,34 @@
 
 
 import express from 'express'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(__dirname+'\\public'))
+
 app.get('/',function(req,res){
-    res.sendFile('C:\\Users\\Admin\\Documents\\New project\\views\\index.html')
+    res.sendFile(__dirname+'\\views\\index.html')
 })
 
 app.get('/about',function(req,res){
-    res.sendFile('C:\\Users\\Admin\\Documents\\New project\\views\\about.html')
+    res.sendFile(__dirname+'\\views\\about.html')
 })
 
 app.get('/resume',function(req,res){
-    res.sendFile('C:\\Users\\Admin\\Documents\\New project\\views\\resume.html')
+    res.sendFile(__dirname+'\\views\\resume.html')
 })
 
 app.get('/services',function(req,res){
-    res.sendFile('C:\\Users\\Admin\\Documents\\New project\\views\\services.html')
+    res.sendFile(__dirname+'\\views\\services.html')
 })
 
 app.get('/contact',function(req,res){
-    res.sendFile('C:\\Users\\Admin\\Documents\\New project\\views\\contact.html')
+    res.sendFile(__dirname+'\\views\\contact.html')
 })
 
 app.listen(5000, function(req,res){
